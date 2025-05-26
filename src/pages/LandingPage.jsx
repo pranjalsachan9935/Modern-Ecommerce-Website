ba991976bf783077152723da65504d31f3af5b65
Pranjal Sachan <146807790+pranjalsachan9935@users.noreply.github.com>
2025-05-26 19:50:50 +0530

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,15 +1,13 @@
-
 import FeaturedProducts from "@/components/sections/FeaturedProducts";
 import Subscribe from "../components/sections/Subscribe";
 import Testimonial from "../components/sections/Testimonial";
 import Slider from "../components/sections/OfferSlider";
 
-
-export default function LandingPage() {
+export default function LandingPage({ searchTerm }) {
   return (
     <div>
-      <Slider/>
-      <FeaturedProducts />
+      <Slider />
+      <FeaturedProducts searchTerm={searchTerm} />
       <Subscribe />
       <Testimonial />
     </div>
